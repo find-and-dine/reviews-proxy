d860995b0d8d4f955dbe58204c027d69c2cb6129
kcelentano <katharinec@gmail.com>
2020-03-23 16:54:40 -0700

    Complete webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
+const path = require('path');
+
 module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+      { test: /\.(js}jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+  resolve: {
+    extensions: ['js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
 };
