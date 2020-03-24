863530bb76406cb812215ef31c1ea5602b6ca6ad
kcelentano <katharinec@gmail.com>
2020-03-23 17:44:20 -0700

    Add App

---
+++ client/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header';
+import Filters from './Filters';
+import ReviewList from './ReviewList';
+import Footer from './Footer';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
