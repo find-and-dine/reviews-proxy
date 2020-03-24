cf3507b9f73b950843bfbdb738e469c041b3f689
kcelentano <katharinec@gmail.com>
2020-03-24 16:11:17 -0700

    Beef up server including implementation of API call

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const cors = require('cors');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/restaurantId', express.static(path.resolve(__dirname, '...', 'public')));
+app.use(cors);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// dummy API call -- refactor later
+app.get('/api/:restaurantId', (req, res) => {
+  const { restaurantId } = req.params;
+  res.send(200);
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
