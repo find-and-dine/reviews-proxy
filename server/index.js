6d7bd812ebd2e3c942ff76c7aeefbc75caf47e80
kcelentano <katharinec@gmail.com>
2020-03-31 10:37:53 -0700

    Latest updates for deployment

--- server/index.js
+++ server/index.js
@@ -9,9 +9,5 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/:restaurantId', express.static(path.resolve(__dirname, '..', 'public')));
 
-// route to get :restaurantId data from service
-
-
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
-// when rquest comes from client, ask proxy server first --> then ping th
